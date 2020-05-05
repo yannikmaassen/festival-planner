660463887bee1bb91e8f0d936ac82c5dd9274810
yannikmaassen <yannik.maassen@t-online.de>
2020-05-05 13:04:47 +0200

    Add changes to create profile

--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -1,31 +1,32 @@
 @extends('layouts.app')
+@include('includes/header')
 
 @section('content')
 <div class="container">
-  <p class="h2 mt-2 text-align">
+  <p class="h2 mt-2">
     Erzähl' was über dich!
   </p>
   <a class="btn-link small text-muted" href="#">Nö, warum?</a>
 
   <form>
     <div class="form-group mt-3">
-      <label for="exampleFormControlInput1">Dein Name</label>
-      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="z.B. Max">
+      <label class="label--form" for="profile_input1">Dein Name</label>
+      <input class="form-control" id="profile_input1" placeholder="z.B. Max">
     </div>
     <div class="form-group">
-      <label for="exampleFormControlFile1">Dein Profilbild</label>
-      <input type="file" class="form-control-file" id="exampleFormControlFile1">
+      <label class="label--form" for="profile_input2">Dein Profilbild</label>
+      <input type="file" class="form-control-file" id="profile_input2">
     </div>
     <div class="form-group">
-      <label for="exampleFormControlTextarea1">Eine kleine Beschreibung über dich</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <label class="label--form" for="profile_input3">Eine kleine Beschreibung über dich</label>
+      <textarea class="form-control" id="profile_input3" rows="3"></textarea>
     </div>
     <div class="form-group">
-      <label for="exampleFormControlTextarea1">Hier Volltextsuche mit Spotify API einbinden</label>
-      <textarea class="form-control" id="exampleFormControlTextarea2" rows="3"></textarea>
+      <label class="label--form" for="profile_input4">Hier Volltextsuche mit Spotify API einbinden</label>
+      <textarea class="form-control" id="profile_input4" rows="3"></textarea>
     </div>
     <div class="form-group">
-      <label for="exampleFormControlSelect1">Auf wie vielen verschiedenen Festivals warst du bereits?</label>
+      <label class="label--form" for="exampleFormControlSelect1">Auf wie vielen verschiedenen Festivals warst du bereits?</label>
       <select class="form-control" id="exampleFormControlSelect1">
         <option>1</option>
         <option>2</option>
@@ -35,12 +36,12 @@
       </select>
     </div>
     <div class="form-group">
-      <label for="exampleFormControlTextarea1">Welche Festivals hast du bereits besucht?</label>
-      <textarea class="form-control" id="exampleFormControlTextarea3" rows="3"></textarea>
+      <label class="label--form" for="profile_input5">Welche Festivals hast du bereits besucht?</label>
+      <textarea class="form-control" id="profile_input5" rows="3"></textarea>
     </div>
     <div class="form-group">
-      <label for="exampleFormControlTextarea1">Deine persönliche Packliste</label>
-      <textarea class="form-control" id="exampleFormControlTextarea4" rows="3" placeholder="Liste hier alle Items auf, die für dich auf keinem Festival fehlen dürfen!"></textarea>
+      <label class="label--form" for="profile_input5">Deine persönliche Packliste</label>
+      <textarea class="form-control" id="profile_input5" rows="3" placeholder="Liste hier alle Items auf, die für dich auf keinem Festival fehlen dürfen!"></textarea>
     </div>
     <div class="form-group row justify-content-center">
       <div class="col-md-8 offset-md-4">
