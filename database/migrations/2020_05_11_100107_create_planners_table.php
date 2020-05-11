d479b53b0540ad0810f3ad5dc59185cb53294663
yannikmaassen <yannik.maassen@t-online.de>
2020-05-11 12:07:41 +0200

    Add models, controllers and migrations

---
+++ database/migrations/2020_05_11_100107_create_planners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planners', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planners');
+    }
+}
