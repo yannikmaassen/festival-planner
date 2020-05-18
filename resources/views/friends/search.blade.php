306e959b85134ec0c43ae98e4e960b3fed2e7082
yannikmaassen <yannik.maassen@t-online.de>
2020-05-18 21:40:54 +0200

    Add styling for search friends

--- resources/views/friends/search.blade.php
+++ resources/views/friends/search.blade.php
@@ -10,15 +10,19 @@
   <div class="row mb-5 d-flex justify-content-start text-center">
     @foreach($friends as $friend)
     <div class="col-md-3">
-      <a href="{{ url('/profile/' . $friend->id) }}" class="d-block border rounded mb-4 p-0 shadow-sm text-decoration-none">
-        <img class="img-fluid mb-3" src="{{ $friend->profile_image }}" />
-        <h4 class="text-muted mb-1">{{ $friend->user_name }}</h4>
-        <h4 class="text-muted mb-1">{{ $friend->email }}</h4>
+      <a href="{{ url('/profile/' . $friend->id) }}" class="d-block border rounded mb-4 p-0 shadow-sm text-decoration-none bg-dark">
+        <h4 class="text-white my-1"><small>User-Name:</small> {{ $friend->user_name }}</h4>
+        <h4 class="text-white my-1"><small>E-Mail:</small> {{ $friend->email }}</h4>
+        <form method="POST" action=""></form>
+        @csrf
+        @method('PATCH')
+        <button class="btn btn-primary my-2">Hinzufügen</button>
       </a>
-
     </div>
     @endforeach
-    <a href="{{ route('friends.add') }}">Abbrechen</a>
+  </div>
+  <div class="row">
+    <a class="btn btn-secondary" href="{{ route('friends.add') }}">Abbrechen</a>
   </div>
 </div>
 @endsection
