de6062d261d8c63fb1051316c42a84f2c79f1d8c
yannikmaassen <yannik.maassen@t-online.de>
2020-05-23 00:58:08 +0200

    Styling searchbars

--- resources/views/playlist/searchResultsPlaylist.blade.php
+++ resources/views/playlist/searchResultsPlaylist.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 <div class="container">
+  <a class="btn-back" href="{{ route('playlist.search', $currentPlanner) }}">
+    <svg class="bi bi-chevron-left mr-2" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 010 .708L5.707 8l5.647 5.646a.5.5 0 01-.708.708l-6-6a.5.5 0 010-.708l6-6a.5.5 0 01.708 0z" clip-rule="evenodd" />
+    </svg>
+    Zurück zur Suche</a>
   <div class="row align-items-center justify-content-center">
     <div class="col-md-9">
-      <h3 class="my-5 text-center">Search Results for "{{ $query }}"</h3>
-      <div class="row">
-        <a class="btn btn-secondary" href="{{ route('playlist.search', $currentPlanner) }}">Abbrechen</a>
-      </div>
+      <h3 class="my-2 text-center text-white">Search Results for "{{ $query }}":</h3>
     </div>
 
   </div>
