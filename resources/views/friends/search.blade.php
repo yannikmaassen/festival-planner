7e7152eff66804bbe5988fd719f47764b6a8bbfb
yannikmaassen <yannik.maassen@t-online.de>
2020-05-26 11:37:15 +0200

    General style for search results

--- resources/views/friends/search.blade.php
+++ resources/views/friends/search.blade.php
@@ -2,30 +2,36 @@
 
 @section('content')
 <div class="container">
-  <div class="row align-items-center">
+  <div class="row align-items-center justify-content-center">
     <div class=" col-md-9">
-      <h3 class="my-5 text-center">Search Results for "{{ $query }}"</h3>
+      <h2 class="my-2 text-center text-white">Suchergebnisse für "{{ $query }}":</h2>
     </div>
+    <a class="btn-back" href="{{ route('friends.add', $currentPlanner) }}">
+      <svg class="bi bi-chevron-left mr-2" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 010 .708L5.707 8l5.647 5.646a.5.5 0 01-.708.708l-6-6a.5.5 0 010-.708l6-6a.5.5 0 01.708 0z" clip-rule="evenodd" />
+      </svg>
+      Zurück zur Suche</a>
   </div>
   <div class="row mb-5 d-flex justify-content-start text-center">
     @foreach($friends as $friend)
     <div class="col-md-3">
-      <a href="{{ url('/profile/' . $friend->id) }}" class="d-block border rounded mb-4 p-0 shadow-sm text-decoration-none bg-dark">
-        <h4 class="text-white my-1"><small>User-Name:</small> {{ $friend->user_name }}</h4>
+      <div class="searchResult__box">
+        <a href="{{ url('/profile/' . $friend->id) }}">
+          <h3 class="text-white my-1">{{ $friend->user_name }}</h3>
+        </a>
         <form method="POST" action="{{ route('planner.update', $currentPlanner) }}">
           <input name="user_id" type="hidden" value="{{ $friend->id }}">
           @csrf
           @method('PATCH')
-          <button class="btn btn-primary my-2" @if($friend->planner()->exists()) disabled @endif>Hinzufügen</button>
+          <button class="btn__add-playlist my-2" @if($friend->planner()->exists()) disabled @endif>
+            <svg class="bi bi-plus-square-fill" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm6.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z" clip-rule="evenodd" />
+            </svg>
+          </button>
         </form>
-      </a>
+      </div>
     </div>
     @endforeach
   </div>
-  <a class="btn-back" href="{{ route('friends.add', $currentPlanner) }}">
-    <svg class="bi bi-chevron-left mr-2" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 010 .708L5.707 8l5.647 5.646a.5.5 0 01-.708.708l-6-6a.5.5 0 010-.708l6-6a.5.5 0 01.708 0z" clip-rule="evenodd" />
-    </svg>
-    Zurück zur Suche</a>
 </div>
 @endsection
--- resources/views/planner/show.blade.php
+++ resources/views/planner/show.blade.php
@@ -53,12 +53,12 @@
         <p>{{ $currentPlanner->festival->genres }}</p>
       </div>
       <section class="festival-info-text">
-        <h4 class="planner-element__heading--dark">
-          Beschreibung
-          <svg class="bi bi-info-circle-fill" width="1.1em" height="1.1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <div class="planner-element__heading--dark">
+          <h4>Beschreibung</h4>
+          <svg class="bi bi-info-circle-fill" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
           </svg>
-        </h4>
+        </div>
         {{ $currentPlanner->info_text }}
       </section>
   </section>
